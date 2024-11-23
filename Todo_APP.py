2ac3884a6d91e278b46e5ec0ebabccaf42ff5efb
_dragos_ <dddconstantin777@gmail.com>
2024-11-23 22:06:05 +0000

    Add time label feature

--- Todo_APP.py
+++ Todo_APP.py
@@ -3,8 +3,7 @@ import functions
 import time
 
 now = time.strftime("%b %d, %Y %H:%M:%S")
-print("It is", now)
-
+print("The time is currently", now)
 
 while True:
     user_action = input("Type 'add', 'show', 'edit', 'complete' or 'exit': ")
